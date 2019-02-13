62203115fa5e74b0a1e65c84013118659290256b
Karim Alweheshy <mail@alweheshy.com>
2019-02-13 08:40:02 +0100

    fix playground

--- UserManagementPlayground.playground/Contents.swift
+++ UserManagementPlayground.playground/Contents.swift
@@ -5,15 +5,35 @@ import PlaygroundSupport
 import UserManagement
 import Networking
 
-let consumer = UserManagementModuleConsumer(presentationBlock: { viewController in
+let consumer = UserManagementModuleConsumer(presentationBlock: { (viewController, _) in
     print("start")
     PlaygroundPage.current.liveView = viewController
-}, dismissBlock: { (viewController) in
+}, dismissBlock: { (viewController, _) in
     print("end")
     PlaygroundPage.current.liveView = nil
 })
 let request = ExplicitLoginRequest(data: ExplicitLoginRequestBody(email: nil, password: nil))
-consumer.execute(request: request) { (result: Result<AuthenticationResponse>) in
+
+final class Networking: NetworkingType {
+    func register(module: Module.Type) {}
+    
+    func execute<T: Codable>(request: InternalRequest,
+                             presentationBlock: ((UIViewController, (() -> Void)?) -> Void)?,
+                             dismissBlock: ((UIViewController, (() -> Void)?) -> Void)?,
+                             completionHandler: @escaping (Result<T>) -> Void) {
+        print("ask for auth token")
+    }
+    
+    func execute<T: Codable>(request: InternalRequest,
+                             completionHandler: @escaping (Result<T>) -> Void) {
+        print("ask for auth token")
+    }
+    
+    func execute<T: Codable>(request: RemoteRequest,
+                             completionHandler: @escaping (Result<T>) -> Void) {}
+}
+
+consumer.execute(networking: Networking(), request: request) { (result: Result<AuthenticationResponse>) in
     print("end")
     PlaygroundPage.current.liveView = nil
 }
